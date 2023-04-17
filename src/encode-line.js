f28e0d9fed5af3027f79fcbcb95935b7ffe5ec55
Yuriy Ulanov <1859697@gmail.com>
2023-04-17 14:51:13 +0300

    encode-line complete

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,11 +10,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  let res = '';
+  let counter = 1;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== str[i + 1]) {
+      if (counter > 1) res += counter;
+      res += str[i];
+      counter = 1;
+    } else {
+      counter++;
+    }
+  }
+  return res;
 }
 
 module.exports = {
-  encodeLine
+  encodeLine,
 };
